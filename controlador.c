5914d7a628bf85ca24034e14646c3374abd89166
lennygin <lennginzan@gmail.com>
2020-02-18 16:53:55 -0600

    starting file

---
+++ controlador.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "modelo.h"
+#include "vista.h"
+
+int main()
+{
+    system("clear");
+
+    char input[50];
+    int length;
+
+    
+
+    getchar();
+    system("clear");
+    return 0;
+}
