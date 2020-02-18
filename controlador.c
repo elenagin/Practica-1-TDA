b41c9e4a4a389071f48a563c776b9d84ab6eaba0
IvanGF199 <39014961+IvanGF199@users.noreply.github.com>
2020-02-18 17:14:10 -0600

    Update

--- controlador.c
+++ controlador.c
@@ -2,12 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "modelo.h"
+//#include "modelo.h"
 #include "vista.h"
 
 int main()
 {
+    int subsN, subsH; 
+    float deltha, fin;
     system("clear");
+
+    vista(&subsN, &subsH, &deltha, &fin);
+
+    printf("%d\n %d\n %f\n %f\n", subsN, subsH, deltha, fin);
 
     char input[50];
     int length;
@@ -15,6 +21,6 @@ int main()
     
 
     getchar();
-    system("clear");
+    //system("clear");
     return 0;
 }
