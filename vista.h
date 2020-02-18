20af9f81e409bef1a0ddbef2aa20eb9d67982eba
IvanGF199 <39014961+IvanGF199@users.noreply.github.com>
2020-02-18 17:30:57 -0600

    Update vista.h

---
+++ vista.h
@@ -0,0 +1,20 @@
+void vista(int *subsN, int *subsH, float *deltha, float *fin )
+{
+	int i;
+	printf("Bienvenido al sistema de prediccion\n");
+	printf("------------------------------------\n");
+	printf("    Pulsa enter para comenzar\n");
+	//scanf("%d", &i);
+	printf("Inserta tus condiciones iniciales para cada plataforma\n\n");
+	printf("Netflix N(0):  ");
+	scanf("%d", subsN);
+	printf("\nHulu H(0):   ");
+	scanf("%d", subsH);
+	system("clear");
+	printf("Inserta como sera el cambio en el tiempo\n");
+	scanf("%f", deltha);
+	system("clear");
+	printf("Inserta el coeficiente que indica hasta cuando se hara el calculo\n");
+	scanf("%f", fin);
+	system("clear");
+}
