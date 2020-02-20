966508aade0a70c691e3ec8000632c23a40e8cfa
IvanGF199 <39014961+IvanGF199@users.noreply.github.com>
2020-02-19 18:10:30 -0600

    Update modelo.h

--- modelo.h
+++ modelo.h
@@ -1,3 +1,22 @@
-void modelo(){
-    
+void modelo(int *subsN, int *subsH, float *deltha, float *fin, float *Nn, float *Hn)
+{
+	float Nt, Ht;
+	float i;
+
+	
+
+	//printf("%f        %f\", *Nn, *Hn);
+	
+		Nt = *Nn + *deltha * (2.5 * *Nn - 5.4 * *Hn);
+	    Ht = *Hn + *deltha * (5.4 * *Hn - 5.4 * *Nn);
+		
+		*Nn= Nt;
+		*Hn= Ht;
+
+		//printf("  %f       %f         %f\n ",i, *Nn, *Hn);
+
+	
+
+	//printf("%f\n", Nt);
+	//printf("%f\n", Ht);
 }
