0684615415d233165f405542ec1af829d4f28d64
IvanGF199 <39014961+IvanGF199@users.noreply.github.com>
2020-02-19 18:11:15 -0600

    Update controlador.c

--- controlador.c
+++ controlador.c
@@ -2,25 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-//#include "modelo.h"
+#include "modelo.h"
 #include "vista.h"
+
 
 int main()
 {
+    FILE *archivo;
+    archivo = fopen("Graf.dat","wt");
+    FILE *archivo1;
+    archivo1 = fopen("Graf1.dat","wt");
     int subsN, subsH; 
-    float deltha, fin;
+    float deltha, fin, Nn, Hn, i;
     system("clear");
 
     vista(&subsN, &subsH, &deltha, &fin);
+    Nn= subsN;
+	Hn= subsH;
 
-    printf("%d\n %d\n %f\n %f\n", subsN, subsH, deltha, fin);
+    for(i=deltha ; i<= fin ; i=i+ deltha)
+	{
+        modelo(&subsN, &subsH, &deltha, &fin, &Nn, &Hn);
+        printf(" %f       %f\n\n", Nn, Hn);
+        fprintf(archivo," %f\t%f\n", i,Nn);
+        fprintf(archivo1," %f\t%f\n", i,Hn);
+    }
 
-    char input[50];
-    int length;
-
-    
 
     getchar();
-    //system("clear");
+  
+    fclose(archivo);
+    fclose(archivo1);
     return 0;
+    
 }
