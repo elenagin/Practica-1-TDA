47a804ed58fe923dc13c64fb6b062807c42a04d2
IvanGF199 <39014961+IvanGF199@users.noreply.github.com>
2020-02-27 20:23:19 -0600

    Update vista.h

--- vista.h
+++ vista.h
@@ -1,31 +1,18 @@
-#include <stdlib.h>
-
-void vista(float *c1_netflix, float *c1_hulu, float *delta, long *max)
+void vista(float *subsN, float *subsH, float *deltha, float *fin )
 {
 	int i;
-	system("clear");
 	printf("Bienvenido al sistema de prediccion\n");
 	printf("------------------------------------\n");
-	printf("   E l e n a   G i n e b r a \n");
-	printf("   M a r i a n a   C a r m o n a \n");
-	printf("   I v á n   G u z m á n \n");
-	printf("------------------------------------\n");
-	printf("Presione enter para continuar\n");
-	getchar();
-	system("clear");
+	printf("    Pulsa enter para comenzar\n");
+	//scanf("%d", &i);
 	printf("Inserta tus condiciones iniciales para cada plataforma\n\n");
-	printf("\nN E T F L I X\nInserte la población inicial: ");
-	scanf(" %f", c1_netflix);
-	getchar();
-	system("clear");
-	printf("H U L U\nInserte la población inicial: ");
-	scanf(" %f", c1_hulu);
-	printf("Presione enter para continuar\n");
-	getchar();
-	system("clear");
-	printf("Inserte ∆t: ");
-	scanf(" %f", delta);
-	printf("Inserte el año límite: ");
-	scanf(" %ld", max);
+	printf("Netflix N(0):  ");
+	scanf("%f", subsN);
+	printf("\nHulu H(0):   ");
+	scanf("%f", subsH);
+	printf("\nInserta como sera el cambio en el tiempo:  ");
+	scanf("%f", deltha);
+	printf("\nInserta el coeficiente que indica hasta cuando se hara el calculo:   ");
+	scanf("%f", fin);
 	system("clear");
 }
